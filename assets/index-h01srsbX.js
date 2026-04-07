df156258d709444ec9c5da4be9748d1d1251b283
kikky <jesusingod79@gmail.com>
2026-04-07 09:49:55 +0900

    플랫폼 추가

---
+++ assets/index-h01srsbX.js
@@ -0,0 +1,2 @@
+import{ay as t,R as e,j as a}from"./index-jmXg9wKL.js";function r(){const{setLayout:s}=t();return e.useEffect(()=>{},[s]),a.jsx("div",{style:{padding:24},children:a.jsx("h1",{children:"Sample Dashboard"})})}export{r as default};
+//# sourceMappingURL=index-h01srsbX.js.map
